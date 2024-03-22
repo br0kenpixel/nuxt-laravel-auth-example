882563a8ccd84932d6949b151669a0a73d9b19fd
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-03-22 22:25:55 +0100

    add registration

--- sanctum-nuxt/pages/register.vue
+++ sanctum-nuxt/pages/register.vue
@@ -2,8 +2,48 @@
 definePageMeta({
     middleware: ['sanctum:guest'],
 });
+
+const client = useSanctumClient();
+
+const form = ref({
+    name: "",
+    email: "",
+    password: ""
+});
+
+async function handleRegister() {
+    const {data, pending, error, refresh} = await useAsyncData("register", () => 
+        client("/register", {
+            method: 'POST',
+            body: {
+                name: form.value.name,
+                email: form.value.email,
+                password: form.value.password,
+                password_confirmation: form.value.password
+            }
+        })
+    );
+
+    if (data.value === undefined && error.value === null) {
+        navigateTo("/");
+    }
+}
 </script>
 
 <template>
-    <p>Register :)</p>
+    <div>
+        <p>Register :)</p>
+
+        <form @submit.prevent="handleRegister">
+            <label for="name" class="name">Name</label>
+            <input type="text" name="name" id="name" v-model="form.name">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" v-model="form.email">
+            <label for="password"></label>
+            <input type="password" name="password" id="password" v-model="form.password">
+
+            <input type="submit" value="Register">
+        </form>
+    </div>
+
 </template>
