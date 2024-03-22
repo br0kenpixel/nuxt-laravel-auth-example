307c0d65db4ff392936d3418952b2b219da2888e
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-03-22 22:41:27 +0100

    add error handling on login

--- sanctum-nuxt/pages/login.vue
+++ sanctum-nuxt/pages/login.vue
@@ -4,22 +4,34 @@ definePageMeta({
 });
 
 const { login } = useSanctumAuth();
+import { FetchError } from 'ofetch';
 
 const form = ref({
     email: 'test@example.com',
     password: 'password',
 });
+const error = ref(null as null|string);
 
 async function handleLogin() {
-    await login({
-        email: form.value.email,
-        password: form.value.password
-    });
+    error.value = null;
+
+    try {
+        await login({
+            email: form.value.email,
+            password: form.value.password
+        });
+    } catch (e) {
+        if (e instanceof FetchError && e.response?.status === 422) {
+            error.value = e.response?._data.message;
+        }
+    }
 }
 </script>
 
 <template>
     <div>
+        <p style="color: red;" v-show="error != null">Error: {{ error }}</p>
+
         <form @submit.prevent="handleLogin">
             <label for="email">Email</label>
             <input type="email" name="email" id="email" v-model="form.email">
